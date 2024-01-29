715447255ba6d0b861f736a803f73727bab8bb33
minasaeedbasta <mina.saeed.basta@gmail.com>
2024-01-29 12:38:32 +0200

    GameLogic : add repainting animation logic

--- main.js
+++ main.js
@@ -37,3 +37,73 @@ var game_over = false,
     move_left = false,
     move_up = false,
     move_down = false;
+
+
+anim_id = requestAnimationFrame(repeat);
+
+function repeat() {
+    // check if the player does'nt lose the game
+    if (game_over === false) {
+        // check collision between the player car and the three coming cars
+        if (isCollided(car, car_1) || isCollided(car, car_2) || isCollided(car, car_3)) {
+            // cancel animation and view and overlay div
+            stopTheGame();
+        }
+        // increase the score counter with every animation refresh
+        score_counter++;
+        // increase the shown score with 1 point every 20 animation refres
+        if (score_counter % 20 == 0) {
+            score.innerText = parseInt(score.innerText) + 1;
+        }
+        // accelerate the movement every 500 animation refresh        
+        if (score_counter % 500 == 0) {
+            speed++;
+            line_speed++;
+        }
+        
+        // repaint the elements with every refresh call
+        car_down(car_1);
+        car_down(car_2);
+        car_down(car_3);
+        line_down(line_1);
+        line_down(line_2);
+        line_down(line_3);
+        line_down(line_4);
+
+        // recursive call to repaint
+        anim_id = requestAnimationFrame(repeat);
+    }
+}
+
+function car_down(car) {
+    var car_current_top = parseInt(window.getComputedStyle(car).getPropertyValue('top'));
+    // check if the car is off the canvas 
+    if (car_current_top > container_height) {
+        // relocate the car to the top of the container and shifted up 200px out of canvas
+        car_current_top = -200;
+        // asign new left property to the car so that it gets a new position with every tine it gets in the canvas
+        var car_left = parseInt(Math.random() * (container_width - car_width - line_width_l));
+        car.setAttribute('style',`left:${car_left}`);
+    }
+    car.setAttribute('style',`top:${car_current_top + speed}`);
+}
+
+function line_down(line) {
+    var line_current_top = parseInt(window.getComputedStyle(line).getPropertyValue('top'));
+    if (line_current_top > container_height) {
+        line_current_top = -200;
+    }
+    line.setAttribute('style',`top:${line_current_top + line_speed}`);
+}
+
+// collision function to be added later
+function isCollided(car1,car2)
+{
+
+}
+
+// stopTheGame function to be added later
+function stopTheGame()
+{
+
+}
