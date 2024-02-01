c1445fa8a4aa831b67fcb8d800d0e1e310533473
Nada-Khater <nadakhater21@gmail.com>
2024-02-01 12:06:51 +0200

    Add Mute button to mute all game sounds in background

--- main.js
+++ main.js
@@ -530,3 +530,37 @@ function showHighScore() {
   // display the high score.
   document.getElementById("highest-score-container").innerText = highScore;
 }
+
+// ================== Mute All Game Sounds ======================
+
+// Get audio elements
+const sounds = [
+  start_counter_sound,
+  engine_start_sound,
+  engine_high_sound,
+  engine_low_sound,
+  car_passed_sound,
+  crash_sound,
+];
+
+// Initialize mute status
+let isMuted = false;
+
+// Toggle mute status
+function Mute() {
+  isMuted = !isMuted;
+  updateMuteButton();
+
+// Toggle mute for each sound
+  sounds.forEach((sound) => {
+    sound.muted = isMuted;
+  });
+}
+
+// Update mute button text
+function updateMuteButton() {
+  const muteBtn = document.getElementById("muteBtn");
+  muteBtn.innerText = isMuted ? " 🔇 " : " 🔊 ";
+}
+
+updateMuteButton();
