d67ad6d7baf3e3a33a75baa77788c6c168de5105
sh-osama-sami <85364511+sh-osama-sami@users.noreply.github.com>
2024-02-03 21:19:21 +0200

    Refactor score handling methods and update score.html

--- js/score.js
+++ js/score.js
@@ -1,118 +1,163 @@
-
 // Scores Handling Methods
 
 function addNewScore(score) {
-    // get the existing scores from the local storage (if any).
-    const existingScores =
-      JSON.parse(localStorage.getItem("carRacingScores")) || [];
-    const currentDate = new Date();
-  
-    // Format the date as a string (e.g., "2024-01-30 12:30:45")
-    const formattedDate = `${padNumber(currentDate.getMonth() + 1)}-${padNumber(
-      currentDate.getDate()
-    )} ${padNumber(currentDate.getHours())}:${padNumber(
-      currentDate.getMinutes()
-    )}`;
-  
-    // append the new score.
-    existingScores.push({ score: score.innerText, date: formattedDate });
-  
-    // save the scores back to the local storage.
-    localStorage.setItem("carRacingScores", JSON.stringify(existingScores));
-  }
-  
-  // Function to pad a number with leading zeros if it's a single digit
-  function padNumber(number) {
-    return number < 10 ? "0" + number : number;
-  }
-  
-  function getScores() {
-    // retrieve the scores from the local storage.
-    const scores = JSON.parse(localStorage.getItem("carRacingScores")) || [];
-  
-    return scores;
-  }
-  
-  function clearScores() {
-    // clear the carRacingScores object.
-    localStorage.removeItem("carRacingScores");
-  
-    // clear the CurrentLevelScore object.
-    localStorage.removeItem("CurrentLevelScore");
-  }
+  // get the existing scores from the local storage (if any).
+  const existingScores =
+    JSON.parse(localStorage.getItem("carRacingScores")) || [];
+  const currentDate = new Date();
 
+  // Format the date as a string (e.g., "2024-01-30 12:30:45")
+  const formattedDate = `${padNumber(currentDate.getMonth() + 1)}-${padNumber(
+    currentDate.getDate()
+  )} ${padNumber(currentDate.getHours())}:${padNumber(
+    currentDate.getMinutes()
+  )}`;
 
+  // append the new score.
+  existingScores.push({ score: score.innerText, date: formattedDate });
 
-  // ========================================
+  // save the scores back to the local storage.
+  localStorage.setItem("carRacingScores", JSON.stringify(existingScores));
+}
+
+// Function to pad a number with leading zeros if it's a single digit
+function padNumber(number) {
+  return number < 10 ? "0" + number : number;
+}
+
+function getScores() {
+  // retrieve the scores from the local storage.
+  const scores = JSON.parse(localStorage.getItem("carRacingScores")) || [];
+
+  return scores;
+}
+
+function clearScores() {
+  // clear the carRacingScores object.
+  localStorage.removeItem("carRacingScores");
+
+  // clear the CurrentLevelScore object.
+  localStorage.removeItem("CurrentLevelScore");
+}
+
+// ========================================
 
 // 1. Handling the Scores Button
 function showScoresInsideTable() {
-    // get the scores from the local storage.
-    const scores = getScores();
-    console.log(scores);
-    // get the table body.
-    const tableBody = document.getElementById("score-table");
-  
-    // clear the table body.
-    tableBody.innerHTML = "";
-  
-    // fill the table body with the scores.
-    scores.forEach((score, index) => {
-      const tableRow = document.createElement("tr");
-      const tableDataIndex = document.createElement("td");
-      const tableDataScore = document.createElement("td");
-      const tableDataDate = document.createElement("td");
-  
-      tableDataIndex.innerText = index + 1;
-      tableDataScore.innerText = score.score;
-      tableDataDate.innerText = score.date;
-  
-      tableRow.appendChild(tableDataIndex);
-      tableRow.appendChild(tableDataDate);
-  
-      tableRow.appendChild(tableDataScore);
-  
-      tableBody.appendChild(tableRow);
-    });
-  }
-  
-  // 2. Handling the Clear Scores Button
-  function clearScoresInsideTable() {
-    clearScores();
-    document.getElementById("clear-message").innerText = "Scores Cleared Successfully !";
-  }
-  function clearScoreMessage() {
-    //confirm the user's intention to clear the scores.
-    document.getElementById("clear-message").innerHTML = `
+  // get the scores from the local storage.
+  const scores = getScores();
+  console.log(scores);
+  // get the table body.
+  const tableBody = document.getElementById("score-table");
+
+  // clear the table body.
+  tableBody.innerHTML = "";
+
+  // fill the table body with the scores.
+  scores.forEach((score, index) => {
+    const tableRow = document.createElement("tr");
+    const tableDataIndex = document.createElement("td");
+    const tableDataScore = document.createElement("td");
+    const tableDataDate = document.createElement("td");
+
+    tableDataIndex.innerText = index + 1;
+    tableDataScore.innerText = score.score;
+    tableDataDate.innerText = score.date;
+
+    tableRow.appendChild(tableDataIndex);
+    tableRow.appendChild(tableDataDate);
+
+    tableRow.appendChild(tableDataScore);
+
+    tableBody.appendChild(tableRow);
+  });
+}
+
+// 2. Handling the Clear Scores Button
+function clearScoresInsideTable() {
+  clearScores();
+  document.getElementById("clear-message").innerText =
+    "Scores Cleared Successfully !";
+}
+function clearScoreMessage() {
+  //confirm the user's intention to clear the scores.
+  document.getElementById("clear-message").innerHTML = `
      <p>Are you sure ?</p>
       <button id="confirm-clear" onclick="clearScoresInsideTable()">Yes</button>
      `;
-  
-    const tableBody = document.getElementById("score-table");
-    // clear the table body.
-    tableBody.innerHTML = "";
+
+  const tableBody = document.getElementById("score-table");
+  // clear the table body.
+  tableBody.innerHTML = "";
+}
+
+//3. high score
+function getHighScore() {
+  // get the scores from the local storage.
+  const scores = getScores();
+
+  // check if there are no scores.
+  if (scores.length === 0) return 0;
+
+  // get the max score.
+  const maxScore = scores.reduce((max, score) => {
+    return Math.max(max, score.score);
+  }, 0);
+
+  return maxScore;
+}
+
+function showHighScore() {
+  // get the high score.
+  const highScore = getHighScore();
+
+  // display the high score.
+  document.getElementById("highest-score-container").innerText = highScore;
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("score-history")
+    .addEventListener("click", function () {
+      updateContent("Score History", "table");
+    });
+
+  document
+    .getElementById("highest-score")
+    .addEventListener("click", function () {
+      updateContent("Highest Score", "highest");
+    });
+
+  document
+    .getElementById("clear-history")
+    .addEventListener("click", function () {
+      updateContent("Clear History", "clear");
+    });
+
+  document.getElementById("back").addEventListener("click", function () {
+    location.href = "index.html";
+  });
+});
+function updateContent(title, content) {
+  document.getElementById("content-title").innerText = title;
+
+  // Hide all content sections
+  document.getElementById("menu-content").style.display = "none";
+  document.getElementById("score-table").style.display = "none";
+  document.getElementById("highest-score-container").style.display = "none";
+  document.getElementById("clear-message").style.display = "none";
+
+  // Show the selected content section
+  if (content === "table") {
+    document.getElementById("menu-content").style.display = "flex";
+    document.getElementById("score-table").style.display = "table";
+    showScoresInsideTable();
+  } else if (content === "highest") {
+    document.getElementById("menu-content").style.display = "flex";
+    document.getElementById("highest-score-container").style.display = "block";
+    showHighScore();
+  } else if (content === "clear") {
+    document.getElementById("menu-content").style.display = "flex";
+    document.getElementById("clear-message").style.display = "block";
+    clearScoreMessage();
   }
-  
-  //3. high score
-  function getHighScore() {
-    // get the scores from the local storage.
-    const scores = getScores();
-  
-    // check if there are no scores.
-    if (scores.length === 0) return 0;
-  
-    // get the max score.
-    const maxScore = scores.reduce((max, score) => {
-      return Math.max(max, score.score);
-    }, 0);
-  
-    return maxScore;
-  }
-  
-  function showHighScore() {
-    // get the high score.
-    const highScore = getHighScore();
-  
-    // display the high score.
-    document.getElementById("highest-score-container").innerText = highScore;
-  }
+}
